ead8c3f2899e4c6e036691124e11ef61ec392ac2
Piush Choudhry <drpiushchoudhry@gmail.com>
2026-03-20 15:25:01 +0530

    fix root route

--- api/index.py
+++ api/index.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
-from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "LMD backend is LIVE 🚀"}
-
-@app.get("/test")
-def test():
-    return {"status": "ok"}
-
-handler = Mangum(app)
