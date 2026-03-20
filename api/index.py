5a768e861bf74d95c21368d1b22f6681e757e9f2
Piush Choudhry <drpiushchoudhry@gmail.com>
2026-03-20 18:40:04 +0530

    added basic auth

--- api/index.py
+++ api/index.py
@@ -1,10 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
 
 app = FastAPI()
+security = HTTPBasic()
+
+USERNAME = "admin"
+PASSWORD = "1234"
+
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, PASSWORD)
+    
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized",
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
 @app.get("/")
-def home():
-    return {"message": "LMD backend is LIVE 🚀"}
+def home(user=Depends(authenticate)):
+    return {"message": "🔐 Secure LMD backend is LIVE 🚀"}
 
 @app.get("/health")
 def health():
