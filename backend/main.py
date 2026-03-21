cd2ea8d0068f76dae57500a9d78a2e7da13c4fd5
Piush Choudhry <drpiushchoudhry@gmail.com>
2026-03-21 10:42:48 +0530

    add analyze endpoint

--- backend/main.py
+++ backend/main.py
@@ -7,23 +7,27 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Backend is running"}
+    return {"message": "Backend running"}
 
 @app.post("/analyze")
 async def analyze(file: UploadFile = File(...)):
     try:
         os.makedirs("uploads", exist_ok=True)
+
         file_path = f"uploads/{file.filename}"
 
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
+        print("Saved file:", file_path)
+
         cap = cv2.VideoCapture(file_path)
 
         if not cap.isOpened():
-            return {"error": "Video cannot be opened"}
+            return {"error": "Cannot open video"}
 
         frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
         cap.release()
 
         return {
